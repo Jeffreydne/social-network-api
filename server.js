d4384e96de0e15b795e12ba1b95452e3fa1beec9
Jeffreydne <Jeffreydne@gmail.com>
2024-03-09 09:05:20 -0800

    set upserver.js to get all users and open app

--- server.js
+++ server.js
@@ -1,5 +1,28 @@
 const express = require('express');
 const db = require('./config/connection');
 
-
+// require models
 const { User } = require('./models');
+
+const PORT = process.env.PORT || 3015
+// set up app variable to be an instance of express, using the methods below
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get('users',
+async (req, res) => {
+    try {
+        const userData = await User.find({});
+        res.status(200).json(userData);
+    } catch (err) {
+        console.log(`ERROR from server.js line 19 ${err}`);
+        res.status(500).json({ message: 'Something is worng in User.find() inserver.js'});
+    }
+})
+
+db.once('open', () => {
+    app.listen(PORT, () => {
+        console.log(`API server running on port ${PORT}!`);
+    });
+});
