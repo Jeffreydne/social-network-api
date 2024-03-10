d6a6957902e0a7f1b1ce3df462fd06ec556a2886
Jeffreydne <Jeffreydne@gmail.com>
2024-03-10 14:24:37 -0700

    add get-one, delete and put routes to User collection on server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const db = require('./config/connection');
 
+const ObjectId = require('mongodb');
 // require models
 const { User } = require('./models');
 
@@ -22,6 +23,17 @@ async (req, res) => {
         res.status(500).json({ message: 'Something is wrong in User.find() inserver.js'});
     }
 });
+// find one user
+app.get('findUser', async (req, res) => {
+    try {
+        const result = await User.findOne({ _id: new ObjectId(req.body._id)
+        });
+        res.status(200).json(result);
+    } catch (err) {
+        console.log(`ERROR from server.js line 26 ${err}`);
+        res.status(500).json({ message: 'Something is wrong in User.findOne() inserver.js'});
+    }
+})
 // Create a new user
 app.post('/newUser', (req,res) => {
     const newUser = new User({
@@ -39,6 +51,34 @@ app.post('/newUser', (req,res) => {
     }
 });
 
+// delete user based on username
+//  TODO change search criteria to _id
+app.delete('/delete', async (req, res) => {
+    try {
+        const result = await User.findOneAndDelete({ username: req.body.username });
+    res.status(200).json(result);
+    console.log(`Deleted: ${result}`);
+    } catch (err) {
+        console.log('Uh Oh, something went wrong line 55 imdex.js app.delete');
+        res.status(500).json({ message: 'something went wrong with delete' });
+    }
+});
+
+// update user info
+// TODO change search criteria to _id
+app.put('/updateUser/:userName', async (req, res) => {
+    try {
+      const result = await User.findOneAndUpdate(
+        {
+        username: req.params.userName 
+      }, {username: req.body.username, email: req.body.email}, {new:true});
+      res.status(200).json(result);
+      console.log(`Updated: ${result}`);
+    } catch (err) {
+      console.log('Uh Oh, something went wrong with line 69 (app.put)');
+      res.status(500).json({ error: 'Something went wrong with update' });
+    }
+});
 db.once('open', () => {
     app.listen(PORT, () => {
         console.log(`API server running on port ${PORT}!`);
